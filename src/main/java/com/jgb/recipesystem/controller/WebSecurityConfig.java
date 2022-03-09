e62bcdb5b3a4e34e2e5938aafa2d648753ffced5
jaimegb <jaime.gotor.blazquez@rabobank.nl>
2022-03-09 20:09:56 +0100

    Added web security config to application

---
+++ src/main/java/com/jgb/recipesystem/controller/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.jgb.recipesystem.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * WebSecurityConfig
+ * <br>
+ * <code>com.jgb.recipesystem.controller.WebSecurityConfig</code>
+ * <br>
+ *
+ * @author Jaime Gotor Blazquez
+ * @since 09 March 2022
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String BASE_PATH = "/recipe-system";
+
+    @Bean
+    protected PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        final String anyPath = BASE_PATH.concat("/**");
+        //super.configure(http);
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, anyPath).hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, anyPath).hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password(this.getPasswordEncoder().encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(this.getPasswordEncoder().encode("password")).roles("USER", "ADMIN");
+    }
+}
