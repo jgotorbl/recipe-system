5fe22d758150746413c27f443782c5995c8cdce8
jaimegb <jaime.gotor.blazquez@rabobank.nl>
2022-03-10 21:44:59 +0100

    Added additional tests for validation

--- src/test/java/com/jgb/recipesystem/controller/RecipeControllerTest.java
+++ src/test/java/com/jgb/recipesystem/controller/RecipeControllerTest.java
@@ -7,6 +7,8 @@ import com.jgb.recipesystem.database.repository.RecipeRepository;
 import com.jgb.recipesystem.model.RecipeDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -157,6 +159,36 @@ public class RecipeControllerTest {
         MvcResult mvcResult = mockMvc.perform(delete("/delete-recipe/{name}", recipeDTO.getName())).andReturn();
         //then
         assertEquals(HttpStatus.NO_CONTENT.value(), mvcResult.getResponse().getStatus());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'{\"name\":\"four_cheese_pizza\",\"vegetarian\":true,\"servings\":1,\"ingredients\":[\"pizza crust\",\"\",\"fontina\",\"parmigiano-reggiano\",\"gorgonzola\"],\"cookingInstructions\":\"put all the ingredients on top of each other and cook in the oven for 15 minutes at 200C\"}'",
+            "'{\"name\":\"four_cheese_pizza\",\"vegetarian\":true,\"servings\":-1,\"ingredients\":[\"pizza crust\",\"mozzarella\",\"fontina\",\"parmigiano-reggiano\",\"gorgonzola\"],\"cookingInstructions\":\"put all the ingredients on top of each other and cook in the oven for 15 minutes at 200C\"}'",
+            "'{\"name\":\"four_cheese_pizza\",\"servings\":1,\"ingredients\":[\"pizza crust\",\"mozzarella\",\"fontina\",\"parmigiano-reggiano\",\"gorgonzola\"],\"cookingInstructions\":\"put all the ingredients on top of each other and cook in the oven for 15 minutes at 200C\"}'",
+            "'{\"name\":\"\",\"vegetarian\":true,\"servings\":1,\"ingredients\":[\"pizza crust\",\"mozzarella\",\"fontina\",\"parmigiano-reggiano\",\"gorgonzola\"],\"cookingInstructions\":\"put all the ingredients on top of each other and cook in the oven for 15 minutes at 200C\"}'"
+    })
+    @WithMockUser(username = "admin", roles = { "ADMIN","USER" })
+    void createRecipe_whenJsonIsNotValid_shouldReturnBadRequest(String json) throws Exception {
+        MvcResult mvcResult =  mockMvc.perform(post("/create-recipe")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)).andReturn();
+        assertEquals(HttpStatus.BAD_REQUEST.value(), mvcResult.getResponse().getStatus());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'{\"name\":\"four_cheese_pizza\",\"vegetarian\":true,\"servings\":1,\"ingredients\":null,\"cookingInstructions\":\"put all the ingredients on top of each other and cook in the oven for 15 minutes at 200C\"}'",
+            "'{\"name\":\"four_cheese_pizza\",\"vegetarian\":true,\"servings\":-1,\"ingredients\":[],\"cookingInstructions\":\"put all the ingredients on top of each other and cook in the oven for 15 minutes at 200C\"}'",
+            "'{\"name\":\"four_cheese_pizza\",\"servings\":null,\"ingredients\":[\"pizza crust\",\"mozzarella\",\"fontina\",\"parmigiano-reggiano\",\"gorgonzola\"],\"cookingInstructions\":\"put all the ingredients on top of each other and cook in the oven for 15 minutes at 200C\"}'",
+            "'{\"name\":null,\"vegetarian\":true,\"servings\":1,\"ingredients\":[\"pizza crust\",\"mozzarella\",\"fontina\",\"parmigiano-reggiano\",\"gorgonzola\"],\"cookingInstructions\":\"put all the ingredients on top of each other and cook in the oven for 15 minutes at 200C\"}'"
+    })
+    @WithMockUser(username = "admin", roles = { "ADMIN","USER" })
+    void updateRecipe_whenRequestIsNotValid_shouldReturnBadRequestCode(String json) throws Exception {
+        MvcResult mvcResult =  mockMvc.perform(post("/update-recipe")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)).andReturn();
+        assertEquals(HttpStatus.BAD_REQUEST.value(), mvcResult.getResponse().getStatus());
     }
 
     private Recipe getRecipeEntity() {
